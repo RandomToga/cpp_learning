ec1d4b5a827fb4609dd60dae5798b26a44bae0e6
RandomToga <maria.09.04.vau@gmail.com>
2025-01-06 16:47:20 +0300

    Add files via upload

---
+++ task_3.cpp
@@ -0,0 +1,21 @@
+/*Напечатайте строки в отсортированном по убыванию порядке. Для сравнения
+строк друг с другом достаточно использовать стандартные операторы сравнения, определённые для std::string.
+Для завершения программы: ctrl+z+enter*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    vector<string> lines;
+    string line;
+    while (getline(cin, line)) {
+        lines.push_back(line);
+    }
+    sort(lines.rbegin(), lines.rend());
+    for (size_t i = 0; i != lines.size(); ++i) {
+        cout << lines[i] << "\n";
+    }
+}
